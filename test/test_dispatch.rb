b3f650c8873b90257164fca12fb5f7f8daf38d4f
Yusuke Endoh <mame@ruby-lang.org>
2026-06-30 00:17:26 +0900

    Always ACK duplicate events to stop Slack resending

--- test/test_dispatch.rb
+++ test/test_dispatch.rb
@@ -29,13 +29,15 @@ class DispatchTest < BotTest
     assert_equal [{ envelope_id: "e1", payload: { text: "hi" } }], @slack.acks
   end
 
-  def test_duplicate_event_id_is_not_reprocessed
+  # A duplicate must still be ACKed so Slack stops resending
+  def test_duplicate_event_is_acked_but_not_reprocessed
     connect
     @slack.deliver(events_msg(event_id: "Ev1", envelope_id: "e1"))
     pump
-    @slack.deliver(events_msg(event_id: "Ev1", envelope_id: "e2"))
+    @slack.deliver(events_msg(event_id: "Ev1", envelope_id: "e2", retry_attempt: 1))
     pump
     assert_equal 1, @calls.size
+    assert_equal [{ envelope_id: "e1" }, { envelope_id: "e2" }], @slack.acks
   end
 
   # The logger must not assume every message has an event_id
